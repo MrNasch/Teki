92a3838a68f78b258ac0a311b554c09b9f2ef710
MrNasch <nicolaspaschal@hotmail.com>
2018-04-10 13:48:21 +0200

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Paschal on 10/04/18.
+//  Copyright © 2018 Nasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
